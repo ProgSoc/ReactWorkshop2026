ab3b3c25f346768f52a2f3f6c8fe69c06a6b0508
BenW <ben@wombattimbers.com.au>
2026-09-10 16:12:17 +1000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+//base line 
+function App() {
+  return (
+  <div>
+    <h1>John Citizen Smith</h1>
+    <p>UTS Student</p>
+    <p>Available for Work</p>
+    <p>
+      Yo whats up, i like learning react and drinking!
+    </p>
+    <div> Skills 
+      <ul>
+        <li> HTML </li>
+        <li> Git </li>
+        <li> JavaFX </li>
+      </ul>
+    </div>
+
+    <div>
+      <button>👍 Like Me (0)</button>
+    </div>
+  </div>
+  );
+}
+export default App;
---
+++ src/mockData.ts
@@ -0,0 +1,7 @@
+export const initialProfile = {
+  name: "John Citizen Smith",
+  age: 21,
+  degree: "UTS Bachelor of Computing Science (Honours)",
+  bio: "I like learning React and eating Fried Brothers and drinking!",
+  skills: ["Git", "Html", "Css", "JavaFX"]
+};
